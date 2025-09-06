5a1f73fd74df18842951d209f72b92382a7f77d6
jimbling <jimbling05@gmail.com>
2025-09-07 06:12:03 +0700

    add : scedule run

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,23 @@
 <?php
 
-use Illuminate\Foundation\Console\ClosureCommand;
+use App\Models\WaQueue;
+use App\Jobs\ProcessWaQueue;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Foundation\Console\ClosureCommand;
 
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    $jobs = WaQueue::where('status', 'pending')
+        ->where('scheduled_at', '<=', now())
+        ->get();
+
+    foreach ($jobs as $job) {
+        ProcessWaQueue::dispatch($job);
+    }
+})->everyMinute();
